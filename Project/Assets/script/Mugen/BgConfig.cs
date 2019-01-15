a6be9e5f9822bf9319fc8c7879836ca2266d3c27
billwillman <billwillman@qq.com>
2019-01-15 20:31:39 +0800

    【增加】BgConfig层配置CS文件

---
+++ Project/Assets/script/Mugen/BgConfig.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Mugen
+{
+    public enum BgType
+    {
+        none,
+        normal,
+        anim
+    }
+
+    public enum TransType
+    {
+        none
+    }
+
+    public class BgInfo
+    {
+        public string name = string.Empty;
+        public BgType type = BgType.none;
+        public TransType trans = TransType.none;
+        public Vector2 delta;
+        public int mask = 1;
+        public int layerno = 0;
+    }
+
+    public class BgStaticInfo : BgInfo
+    {
+        public int tile = 0;
+    }
+
+    public class BgConfig
+    {
+
+    }
+}
