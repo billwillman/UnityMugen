176bbfaed5f46b38aeeff32d28291559ac679c72
billwillman <billwillman@qq.com>
2019-01-28 17:39:06 +0800

    11

--- Project/Assets/script/Mugen/CNSConfig.cs
+++ Project/Assets/script/Mugen/CNSConfig.cs
@@ -10,9 +10,9 @@ namespace Mugen
 		//public static readonly float _cPerUnit = 0.8f;
 		private Dictionary<int, CNSStateDef> m_StateDefMap = null;
 		private Dictionary<string, int> m_StateNameIdMap = null;
-		private static int m_GlobalId = -1;
+		private  int m_GlobalId = -1;
 
-		private static int NewGlobalId()
+		private int NewGlobalId()
 		{
 			return ++m_GlobalId;
 		}
