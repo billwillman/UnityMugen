88bf50ba5473b10d298d304d736dc1a7d6e1f4d5
billwillman <billwillman@qq.com>
2019-02-13 19:20:16 +0800

    1

--- Project/Assets/script/Mugen/CNSState.cs
+++ Project/Assets/script/Mugen/CNSState.cs
@@ -4,6 +4,25 @@ using LuaInterface;
 
 namespace Mugen
 {
+
+    public enum CnsStateType
+    {
+        none = 0,
+        // 残影
+        AfterImage,
+        // 残影时间
+        AfterImageTime,
+        AllPalFX,
+        // 角度加
+        AngleAdd,
+        // 角度绘制
+        AngleDraw,
+        // 角度乘
+        AngleMul,
+        // 角度设置
+        AngleSet,
+    }
+
     public class CNSState
     {
         private string m_Name = string.Empty;
