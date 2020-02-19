b3526df3f89cc9d04b9370c9a043fa434742b748
billwillman <billwillman@qq.com>
2020-02-19 19:33:00 +0800

    2

--- Project/Assets/AIEditor/Editor/AIGraph.cs
+++ Project/Assets/AIEditor/Editor/AIGraph.cs
@@ -1,8 +1,26 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 using System.Collections;
 using XNode;
+using XNodeEditor;
 
 [CreateAssetMenu]
 public class AIGraph : NodeGraph {
 
+}
+
+[CustomNodeGraphEditor(typeof(AIGraph))]
+public class AINodeGraphEditor: NodeGraphEditor
+{
+	public override void AddContextMenuItems(GenericMenu menu)
+	{
+		base.AddContextMenuItems (menu);
+		menu.AddItem (new GUIContent ("导出Lua"), false,
+			ExportToLua
+		);
+	}
+
+	private void ExportToLua()
+	{
+	}
 }
