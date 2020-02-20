80fc0120e1df06d3799417836393064079e99f98
billwillman <billwillman@qq.com>
2020-02-21 00:03:18 +0800

    222

--- Project/Assets/AIEditor/Editor/AIGraph.cs
+++ Project/Assets/AIEditor/Editor/AIGraph.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using System.Collections;
 using XNode;
 using XNodeEditor;
+using LitJson;
+using Utils;
 
 [CreateAssetMenu]
 public class AIGraph : NodeGraph {
@@ -30,6 +32,31 @@ public class AINodeGraphEditor: NodeGraphEditor
 
 	private void ExportToJson()
 	{
-		
+		/*
+		var target = this.target;
+		if (target == null)
+			return;
+		var lst = target.nodes;
+		if (lst != null) {
+			
+			string name = target.name;
+			if (!string.IsNullOrEmpty (name)) {
+				string str = LitJson.JsonMapper.ToJson (lst);
+				string fileName = string.Format ("Assets/AIEditor/{0}.json", name);
+				System.IO.FileStream stream = new System.IO.FileStream (fileName, System.IO.FileMode.Create, System.IO.FileAccess.Write);
+				try
+				{
+					FilePathMgr.GetInstance().WriteString(stream, str);
+					Debug.LogFormat("[ExportToJson] {0} Saved~!", fileName);
+				} finally {
+					stream.Close ();
+					stream.Dispose ();
+				}
+			} else {
+				Debug.LogError ("[ExportToJson] name is null");
+			}
+
+		}
+		*/
 	}
 }
