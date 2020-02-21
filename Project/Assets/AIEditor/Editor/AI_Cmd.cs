db0c104c00f3047d1ab1b2ac80fb939494c4c7b1
billwillman <billwillman@qq.com>
2020-02-22 06:16:39 +0800

    1

--- Project/Assets/AIEditor/Editor/AI_Cmd.cs
+++ Project/Assets/AIEditor/Editor/AI_Cmd.cs
@@ -666,6 +666,19 @@ public class AI_CreateStateDef: AI_BaseNode
 		if (!string.IsNullOrEmpty(animate))
 			ret += string.Format ("\t\tdef.Animate = {0}\n\r", animate);
 
+		// 创建State
+		var pp = this.GetPort("output");
+		if (pp != null) {
+			for (int i = 0; i < pp.ConnectionCount; ++i) {
+				var n = pp.GetConnection (i);
+				if (n == null || n.node == null)
+					continue;
+				if (n.node is AI_CreateStateEvent) {
+					ret += n.node.ToString ();
+				}
+			}
+		}
+
 		return ret;
 	}
 
@@ -699,20 +712,23 @@ public class AI_CreateStateDef: AI_BaseNode
 	}
 
 	public override void OnCreateConnection(NodePort from, NodePort to) { 
-		
-		if (from != null && from.node.GetType () == typeof(AI_Cmd)) {
-			DoCreateConnect<AI_Cmd> (from, ref input, "input"); 
-		} else if (from != null && from.node.GetType () == typeof(AI_StateEvent_PlayCns)) {
-			DoCreateConnect<AI_StateEvent_PlayCns> (from, ref prevCns, "prevCns");
-		} else {
-			from.Disconnect (to);
+
+		if (from != null && from.node != this) {
+			if (from != null && from.node.GetType () == typeof(AI_Cmd)) {
+				DoCreateConnect<AI_Cmd> (from, ref input, "input"); 
+			} else if (from != null && from.node.GetType () == typeof(AI_StateEvent_PlayCns)) {
+				DoCreateConnect<AI_StateEvent_PlayCns> (from, ref prevCns, "prevCns");
+			} else {
+				from.Disconnect (to);
+			}
 		}
 	}
 
 	public override void OnRemoveConnection(NodePort port)
 	{
-		if (port.direction != NodePort.IO.Input)
+		if (port.direction != NodePort.IO.Input) {
 			return;
+		}
 
 		if (port.ValueType == typeof(AI_Cmd)) {
 			DoDisConnect<AI_Cmd> (port, ref input);
@@ -725,6 +741,22 @@ public class AI_CreateStateDef: AI_BaseNode
 //[CreateNodeMenu("AI/创建StateEvent")]
 public abstract class AI_CreateStateEvent: AI_BaseNode
 {
+	public static string VaildStr(float v)
+	{
+		if (Mathf.Abs (v - CNSStateDef._cNoVaildVelset) <= float.Epsilon)
+			return "nil";
+		else
+			return v.ToString ();
+	}
+
+	public static string VaildStr(int v)
+	{
+		if (v == CNSStateDef._cNoVaildVelset)
+			return "nil";
+		else
+			return v.ToString ();
+	}
+
 	[Input(ShowBackingValue.Never, ConnectionType.Override)]
 	public AI_CreateStateDef parent;
 
@@ -735,9 +767,21 @@ public abstract class AI_CreateStateEvent: AI_BaseNode
 
 	public CnsStateTriggerType triggleType = CnsStateTriggerType.AnimElem;
 
+	public override void OnRemoveConnection(NodePort port)
+	{
+		if (port.direction != NodePort.IO.Input)
+			return;
+
+		if (port.ValueType == typeof(AI_CreateStateDef)) {
+			DoDisConnect<AI_CreateStateDef> (port, ref parent);
+		} else if (port.ValueType == typeof(AI_BaseCondition)) {
+			DoDisConnect<AI_BaseCondition> (port, ref condition);
+		}
+	}
+
 	public override void OnCreateConnection(NodePort from, NodePort to)
 	{
-		//if (from.node == this) {
+		if (from.node != this) {
 			if (from != null && (from.node.GetType () == typeof(AI_Cond_PlayerTime) ||
 			   from.node.GetType () == typeof(AI_Cond_PlayerAniTime))) {
 				triggleType = CnsStateTriggerType.AnimTime;
@@ -745,7 +789,12 @@ public abstract class AI_CreateStateEvent: AI_BaseNode
 			          (from.node.GetType () == typeof(AI_Cond_PlayerAnimElemTime))) {
 				triggleType = CnsStateTriggerType.AnimElem;
 			}
-		//}
+
+			DoCreateConnect (from, ref parent, "parent");
+			DoCreateConnect (from, ref condition, "condition");
+		}
+
+
 	}
 
 	public override object GetValue(NodePort port)
@@ -775,7 +824,12 @@ public abstract class AI_CreateStateEvent: AI_BaseNode
 				ret += condition.ToCondString ("luaPlayer");
 			}
 
-			ret += "\t\t" + doStr;
+			ret += "\t\t\t\t" + doStr;
+
+			if (setPersistent) {
+				ret += "\t\t\t\ttrigger:Persistent(luaPlayer, state, true)\n\r";
+			}
+			ret += "\t\tend\n\r";
 		}
 
 		return ret;
@@ -787,6 +841,12 @@ public class AI_StateEvent_PlaySnd: AI_CreateStateEvent
 {
 	public int group;
 	public int index;
+
+	protected override string GetDoStr()
+	{
+		string ret = string.Format ("trigger:PlaySnd(luaPlayer, {0:D}, {1:D}", group, index);
+		return ret;
+	}
 }
 
 [CreateNodeMenu("AI/创建StateEvent/设置速度")]
@@ -794,6 +854,12 @@ public class AI_StateEvent_VelSet: AI_CreateStateEvent
 {
 	public float velx = CNSStateDef._cNoVaildVelset;
 	public float vely = CNSStateDef._cNoVaildVelset;
+
+	protected override string GetDoStr()
+	{
+		string ret = string.Format ("trigger:VelSet(luaPlayer, {0}, {1})", VaildStr (velx), VaildStr (vely));
+		return ret;
+	}
 }
 
 [CreateNodeMenu("AI/创建StateEvent/速度增加")]
@@ -801,6 +867,12 @@ public class AI_StateEvent_VelAdd: AI_CreateStateEvent
 {
 	public float velx = CNSStateDef._cNoVaildVelset;
 	public float vely = CNSStateDef._cNoVaildVelset;
+
+	protected override string GetDoStr()
+	{
+		string ret = string.Format ("trigger:VelAdd(luaPlayer, {0}, {1})", VaildStr (velx), VaildStr (vely));
+		return ret;
+	}
 }
 
 [CreateNodeMenu("AI/创建StateEvent/位置增加")]
@@ -808,6 +880,12 @@ public class AI_StateEvent_PosAdd: AI_CreateStateEvent
 {
 	public int x = CNSStateDef._cNoVaildVelset;
 	public int y = CNSStateDef._cNoVaildVelset;
+
+	protected override string GetDoStr()
+	{
+		string ret = string.Format ("trigger:PosAdd(luaPlayer, {0}, {1})", VaildStr (x), VaildStr (y));
+		return ret;
+	}
 }
 
 [CreateNodeMenu("AI/创建StateEvent/设置位置")]
@@ -815,18 +893,36 @@ public class AI_StateEvent_PosSet: AI_CreateStateEvent
 {
 	public int x = CNSStateDef._cNoVaildVelset;
 	public int y = CNSStateDef._cNoVaildVelset;
+
+	protected override string GetDoStr()
+	{
+		string ret = string.Format ("trigger:PosSet(luaPlayer, {0}, {1})", VaildStr (x), VaildStr (y));
+		return ret;
+	}
 }
 
 [CreateNodeMenu("AI/创建StateEvent/StateType设置")]
 public class AI_StateEvent_StateTypeSet: AI_CreateStateEvent
 {
 	public Cns_Type stateType = Cns_Type.S;
+
+	protected override string GetDoStr()
+	{
+		string ret = string.Format ("trigger:StateTypeSet(luaPlayer, {0}.{1})", stateType.GetType().FullName, stateType.ToString());
+		return ret;
+	}
 }
 
 [CreateNodeMenu("AI/创建StateEvent/Ctrl设置")]
 public class AI_StateEvent_CtrlSet: AI_CreateStateEvent
 {
 	public int ctrl = 1;
+
+	protected override string GetDoStr()
+	{
+		string ret = string.Format ("trigger:CtrlSet(luaPlayer, {0:D})", ctrl);
+		return ret;
+	}
 }
 
 [CreateNodeMenu("AI/创建StateEvent/创建爆炸")]
@@ -851,7 +947,13 @@ public class AI_StateEvent_PlayAni: AI_CreateStateEvent
 {
 	public int anim = CNSStateDef._cNoVaildAnim;
 	public bool isLoop = false;
-	public bool isCleanStateDef = true;
+	//public bool isCleanStateDef = true;
+
+	protected override string GetDoStr()
+	{
+		string ret = string.Format ("trigger:PlayAnim(luaPlayer, {0:D}, {1})", anim, isLoop.ToString());
+		return ret;
+	}
 }
 
 [CreateNodeMenu("AI/创建StateEvent/切换Cns状态")]
@@ -868,11 +970,18 @@ public class AI_StateEvent_PlayCns: AI_CreateStateEvent
 			return nextStateDef;
 		return null;
 	}
+		
 }
 
 [CreateNodeMenu("AI/创建StateEvent/切换Stand状态")]
 public class AI_StateEvent_PlayStandCns: AI_CreateStateEvent
-{}
+{
+	protected override string GetDoStr()
+	{
+		string ret = "trigger:PlayStandCns(luaPlayer)";
+		return ret;
+	}
+}
 
 [CreateNodeMenu("AI/创建StateEvent/创建飞行物")]
 public class AI_StateEvent_CreateProj: AI_CreateStateEvent
@@ -899,19 +1008,33 @@ public class AI_StateEvent_CreateProj: AI_CreateStateEvent
 public class AI_StateEvent_RemoveExplod: AI_CreateStateEvent
 {
 	public int id;
+
+	protected override string GetDoStr()
+	{
+		string ret = string.Format ("trigger:RemoveExplod(luaPlayer, {0:D})", id);
+		return ret;
+	}
 }
 
+/*
 [CreateNodeMenu("AI/创建StateEvent/切换调色板")]
 public class AI_StateEvent_ChangePallet: AI_CreateStateEvent
 {
 	public int index;
 }
+*/
 
 [CreateNodeMenu("AI/创建StateEvent/设置float var")]
 public class AI_StateEvent_floatVarSet: AI_CreateStateEvent
 {
 	public int index;
 	public float value;
+
+	protected override string GetDoStr()
+	{
+		string ret = string.Format ("trigger:fVarSet(luaPlayer, {0:D}, {1})", index, value.ToString());
+		return ret;
+	}
 }
 
 [CreateNodeMenu("AI/创建StateEvent/设置int var")]
@@ -919,12 +1042,24 @@ public class AI_StateEvent_intVarSet: AI_CreateStateEvent
 {
 	public int index;
 	public int value;
+
+	protected override string GetDoStr()
+	{
+		string ret = string.Format ("trigger:VarSet(luaPlayer, {0:D}, {1:D})", index, value);
+		return ret;
+	}
 }
 
 [CreateNodeMenu("AI/创建StateEvent/角色暂停")]
 public class AI_StateEvent_PlayerPause: AI_CreateStateEvent
 {
 	public float time;
+
+	protected override string GetDoStr()
+	{
+		string ret = string.Format ("trigger:Pause(luaPlayer, {0})", time.ToString());
+		return ret;
+	}
 }
 
 [CreateNodeMenu("AI/创建StateEvent/速度乘法")]
@@ -932,8 +1067,19 @@ public class AI_StateEvent_VelMul: AI_CreateStateEvent
 {
 	public float velX = CNSStateDef._cNoVaildVelset;
 	public float velY = CNSStateDef._cNoVaildVelset;
+
+	protected override string GetDoStr()
+	{
+		string ret = string.Format ("trigger:VelMul(luaPlayer, {0}, {1})", VaildStr (velX), VaildStr (velY));
+		return ret;
+	}
 }
 
 [CreateNodeMenu("AI/创建StateEvent/删除角色自己")]
 public class AI_StateEvent_PlayerDestroySelf: AI_CreateStateEvent
-{}
+{
+	protected override string GetDoStr()
+	{
+		return "trigger:DestroySelf(luaPlayer)";
+	}
+}
