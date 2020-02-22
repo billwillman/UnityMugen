baf94268ae3d5a5fe0e561973a42e5cdb386f227
billwillman <billwillman@qq.com>
2020-02-22 17:45:09 +0800

    1

--- Project/Assets/AIEditor/Editor/AI_Cmd.cs
+++ Project/Assets/AIEditor/Editor/AI_Cmd.cs
@@ -614,7 +614,20 @@ public class AI_Cond_PlayerAnimElemTime: AI_BaseCondition
 	}
 }
 
+[CreateNodeMenu("AI/条件/Random")]
+public class AI_Cond_Random: AI_BaseCondition
+{
+	public int randomValue = 1000;
+	public int value = 500;
+	public AI_Cond_Op op = AI_Cond_Op.Less;
 
+	public override string ToCondString(string luaPlayer)
+	{
+		var opStr = GetOpStr (op);
+		string ret = string.Format ("trigger:Random({0:D}){1}{2:D}", randomValue, opStr, value);
+		return ret;
+	}
+}
 
 [CreateNodeMenu("AI/创建StateDef")]
 public class AI_CreateStateDef: AI_BaseNode
