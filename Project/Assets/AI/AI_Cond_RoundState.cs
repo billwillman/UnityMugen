ef36e1577428db77f85180ef993f036ee4cf6ef9
billwillman <billwillman@qq.com>
2020-02-26 21:13:54 +0800

    1

---
+++ Project/Assets/AI/AI_Cond_RoundState.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace XNode.Mugen {
+
+    [CreateNodeMenu("AI/条件/游戏回合")]
+    [System.Serializable]
+    public class AI_Cond_RoundState : AI_BaseCondition {
+
+        [SerializeField]
+        public int value = 0;
+        [SerializeField]
+        public AI_Cond_Op op = AI_Cond_Op.Equal;
+
+        public override string ToCondString(string luaPlayer) {
+            var opStr = GetOpStr(op);
+            string ret = string.Format("RoundState{0}{1:D}", opStr, value);
+            return ret;
+        }
+    }
+
+}
